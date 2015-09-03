3d23271d82abf1b083dc67e37451cd8589237362
Kocsen <kocsenc@gmail.com>
2015-09-03 15:10:32 -0400

    Documentation, more h4x

--- Driver.py
+++ Driver.py
@@ -1,6 +1,5 @@
 __author__ = "kocsenc"
 
-from sys import argv
 import re
 import getpass
 import hashlib
@@ -10,41 +9,47 @@ from SaveAdapters.FileAdapter import FileAdapter
 
 def main():
     print(
-        "Welcome to the RIT ID collection system. Data is encrypted with SHA-256.\n"
+        "Welcome to the RIT ID check-in system. Data is encrypted with SHA-256.\n"
         "It cannot be decrypted back.\n"
         "Press 'q' to quit\n")
 
     adapter = FileAdapter('out.csv')
-    existing_students = adapter.read_all()
 
-    collect(FileAdapter('out.csv'))
+    collect(adapter)
 
 
 def collect(adapter):
-    raw_id = getpass.getpass("Please swipe your RIT ID.").strip()
-
+    raw_id = getpass.getpass("Please swipe your RIT ID").strip()
     regex = re.compile('^;(?P<student_id>\d{9})=(?P<issue_number>\d+)\?$')
-
     match = re.search(regex, raw_id)
+
     if match:
         student_id = match.group("student_id")
         hashed_student_id = hashlib.sha256(str.encode(student_id)).hexdigest()
-
         issue_number = match.group("issue_number")[0]
 
-        fname = input("First Name: ")
-        lname = input("Last Name: ")
-        rit_username = input("RIT username: ")
+        if hashed_student_id not in adapter.student_dict:
+            print("Welcome, since it is your first time, we need some more info.")
 
-        print("hash id:\t", hashed_student_id)
-        print("issue:\t\t", issue_number)
-        print("Name:\t\t", fname, lname)
-        print("RIT username:\t", rit_username)
+            fname = input("First Name: ")
+            lname = input("Last Name: ")
+            rit_username = input("RIT username: ")
 
-        adapter.save(hashed_student_id, fname, lname, rit_username, issue_number)
+            # print("Name:\t\t", fname, lname)
+            # print("RIT username:\t", rit_username)
+
+            adapter.save(hashed_student_id, fname, lname, rit_username, issue_number)
+
+        check_in(adapter.student_dict[hashed_student_id])
 
     else:
-        print("error")
+        print("Try again.\n")
+
+
+def check_in(student):
+    # TODO: Perform check in action
+
+    print("Thanks, %s. You are good to go! \t=====>" % student.first_name)
 
 
 if __name__ == "__main__":
--- SaveAdapters/FileAdapter.py
+++ SaveAdapters/FileAdapter.py
@@ -7,7 +7,7 @@ class FileAdapter(AdapterInterface):
     """
     Adapter to use if you want to save things as a file
 
-    FORMAT:
+    CSV FORMAT:
     hashed,fname,lname,rit_username,issue_number
     """
 
@@ -17,27 +17,30 @@ class FileAdapter(AdapterInterface):
         :param filename:
         :return:
         """
-        self.student_dict = None
         self.filename = filename
+        self.student_dict = self.read_all()
 
     def save(self, hashed_sid, fname, lname, rit_username, issue_number):
         with open(self.filename, 'a') as f:
             to_write = ','.join([hashed_sid, fname, lname, rit_username, issue_number])
             f.write(to_write + '\n')
 
+            # Save to the Student Dictionary
+            self.student_dict[hashed_sid] = Student(hashed_sid, fname, lname, rit_username, issue_number)
+
     def read_all(self):
-        return_dict = {}
+        students_dict = {}
         try:
             with open(self.filename, 'r') as f:
                 for line in f.readlines():
                     line = line.strip().split(',')
 
                     student_object = Student(line[0], line[1], line[2], line[3], line[4])
-                    return_dict[line[0]] = student_object
+                    students_dict[line[0]] = student_object
 
-            self.student_dict = return_dict
-            return return_dict
-        except FileNotFoundError:
+            self.student_dict = students_dict
+            return students_dict
+        except IOError:
             return {}
 
     def read_one(self, hashed_sid):
